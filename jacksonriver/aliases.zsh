d830bbd76b9b5ff6d79bfd83742eade4c81bbb92
David Barbarisi <davidbarbarisi@gmail.com>
2015-02-03 15:07:55 -0500

    cleaned up some aliases and added workday.

--- jacksonriver/aliases.zsh
+++ jacksonriver/aliases.zsh
@@ -1,16 +1,14 @@
+alias -r workday="open /Applications/Skype.app ; open /Applications/HipChat.app ; open /Applications/Harvest.app"
 
 dru() { php -l "$*" && phpcs --standard=Drupal "$*"}
-
-# This conflicts with a mkdir alias.
-# md() { phpmd "$*" text codesize,design,naming,unusedcode }
 
 sb4() { open https://api.gospringboard.com/api/sb/4/search/$* }
 
 alias -r work="cd ~/Development/work"
 alias -r play="cd ~/Development/personal"
 
-alias -r sb4up="cd ~/Development/work/sb4 && vagrant up"
-alias -r sb4down="cd ~/Development/work/sb4 && vagrant suspend"
+alias -r sb4up="cd ~/Development/work/sb4dev && vagrant up"
+alias -r sb4down="cd ~/Development/work/sb4dev && vagrant halt"
 
 # Harvest
 alias -r day-start="hcl start ops Day start and email"
@@ -20,4 +18,3 @@ alias -r platform-standup="hcl start sb4meetings Platform standup call"
 alias -r p2p-checkin="hcl start p2pmeetings P2P status call"
 
 sb4dev() { hcl start sb4dev $* }
-p2pdev() { hcl start p2pdev $* }
