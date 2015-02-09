6f4caed402acfb52da8d988b20eb65830dde3b5a
David Barbarisi <davidbarbarisi@gmail.com>
2015-02-09 11:12:11 -0500

    fixed the phpcbf alias

--- drupal/aliases.zsh
+++ drupal/aliases.zsh
@@ -1,4 +1,4 @@
 
 dru() { php -l "$*" && phpcs --standard=Drupal "$*"}
 
-dfix() { phpcbf --standard=Drupal --tab-width=2 --encoding=UTF-8 }
+dfix() { phpcbf --standard=Drupal --tab-width=2 --encoding=UTF-8 "$*" }
